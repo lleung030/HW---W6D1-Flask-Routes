c3ccfbcd52e3d483e4f845b6cafc037f84fcac78
user <user@Users-Mac.local>
2022-12-05 22:48:36 -0800

    first commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
