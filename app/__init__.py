70482a39025bfafde69d895b33ee015f9ca53e65
user <user@Users-Mac.local>
2022-12-07 23:37:19 -0800

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,26 @@
 from flask import Flask 
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 app = Flask(__name__)
 
-from app import routes
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+# from app import routes, models
+from app.blueprint.main import bp as main_bp
+from app.blueprint.blog import bp as blog_bp
+from app.blueprint.auth import bp as auth_bp
+app.register_blueprint(main_bp)
+app.register_blueprint(blog_bp)
+app.register_blueprint(auth_bp)
+
+
+
+
---
+++ app/blueprint/auth/routes.py
@@ -0,0 +1,10 @@
+from . import bp as app 
+from flask import render_template
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
